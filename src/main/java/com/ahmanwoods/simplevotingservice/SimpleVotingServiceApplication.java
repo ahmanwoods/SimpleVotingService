9587f71383b0250e9bbd5c003c6d8013085e29d0
Ahman Woods <nero@Ahmans-MacBook-Pro.local>
2022-04-03 15:35:48 -0600

    Initial commit

---
+++ src/main/java/com/ahmanwoods/simplevotingservice/SimpleVotingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ahmanwoods.simplevotingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVotingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleVotingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmanwoods/simplevotingservice/SimpleVotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmanwoods.simplevotingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleVotingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
